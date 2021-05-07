ce65de7b0b01205943ea6eeb7a14fdf5360504fb
unknown <ijlnflhbrzdev@gmail.com>
2021-05-07 13:56:53 +0700

    Dasar dari Application Shell sebenarnya sudah berhasil dibuat walaupun belum sempurna karena tombol drawer masih belum dapat berfungsi, dan komponen UI belum dapat diakses dalam keadaan offline. Tapi tak apa, kita akan melakukannya secara perlahan.

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,10 @@
+/*
+
+     INI ADALAH FILE ENTRII POINT => Lebih Jelas nya silahkan cek ulang materi pada FUNDAMENTAL WEB FRONT END
+
+*/
 import "regenerator-runtime";
+import "../style/style.css";
+import "../style/responsive.css";
 
 console.log("Hello Coders!");
