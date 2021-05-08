864a7ae98208eac24ed3b8c08e1636304a8f5a33
unknown <ijlnflhbrzdev@gmail.com>
2021-05-08 20:35:19 +0700

    Pada Methon afterRender menghapus seluruh consol.log() lalu menganti code yg ada untuk menampilkan data movie atau movies melalui fungsi template-creator yg sudah di buat sebelumnya pada folder Templates

--- src/scripts/views/pages/detail.js
+++ src/scripts/views/pages/detail.js
@@ -1,19 +1,19 @@
 import UrlParser from "../../routes/url-parser";
 import TheMovieDbSource from "../../data/themoviedb-source";
+import { createMovieDetailTemplate } from "../templates/template-creator";
 
 const Detail = {
   async render() {
     return `
-          <div id="movie" class="movie"></div>
+      <div id="movie" class="movie"></div>
     `;
   },
 
   async afterRender() {
     const url = UrlParser.parseActiveUrlWithoutCombiner();
     const movie = await TheMovieDbSource.detailMovie(url.id);
-    console.log(movie);
-
-    // TODO: tampilkan movie di dalam DOM
+    const movieContainer = document.querySelector("#movie");
+    movieContainer.innerHTML = createMovieDetailTemplate(movie);
   },
 };
 
--- src/scripts/views/pages/upcoming.js
+++ src/scripts/views/pages/upcoming.js
@@ -1,22 +1,24 @@
 import TheMovieDbSource from "../../data/themoviedb-source";
+import { createMovieItemTemplate } from "../templates/template-creator";
 
 const Upcoming = {
   async render() {
     return `
-        <div class="content">
-          <h2 class="content__heading">Upcoming in Cinema</h2>
-            <div id="movies" class="movies">
+      <div class="content">
+        <h2 class="content__heading">Upcoming in Cinema</h2>
+        <div id="movies" class="movies">
 
-            </div>
         </div>
+      </div>
     `;
   },
 
   async afterRender() {
     const movies = await TheMovieDbSource.upcomingMovies();
-    console.log(movies);
-
-    // TODO: tampilkan movies di dalam DOM
+    const moviesContainer = document.querySelector("#movies");
+    movies.forEach((movie) => {
+      moviesContainer.innerHTML += createMovieItemTemplate(movie);
+    });
   },
 };
 
