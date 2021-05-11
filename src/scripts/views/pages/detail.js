1d828027b84bc882aeee1b8ba9d539f5dd365365
unknown <ijlnflhbrzdev@gmail.com>
2021-05-11 20:29:01 +0700

    Problem  Solving Pada like button tidak muncul sekarang mucul

--- src/scripts/views/pages/detail.js
+++ src/scripts/views/pages/detail.js
@@ -1,12 +1,13 @@
 import UrlParser from "../../routes/url-parser";
 import TheMovieDbSource from "../../data/themoviedb-source";
 import { createMovieDetailTemplate, createLikeButtonTemplate } from "../templates/template-creator";
+import LikeButtonInitiator from "../../utils/like-button-intiator";
 
 const Detail = {
   async render() {
     return `
       <div id="movie" class="movie"></div>
-      <div id="likeButtonContainer>
+      <div id="likeButtonContainer">
 
       </div>
     `;
@@ -18,8 +19,19 @@ const Detail = {
     const movieContainer = document.querySelector("#movie");
     movieContainer.innerHTML = createMovieDetailTemplate(movie);
 
-    const likeButtonContainer = document.getElementById("likeButtonContainer");
-    likeButtonContainer.innerHTML += createLikeButtonTemplate();
+    const likeButtonContainer = document.querySelector("#likeButtonContainer");
+    likeButtonContainer.innerHTML = createLikeButtonTemplate();
+
+    LikeButtonInitiator.init({
+      LikeButtonContainer: document.querySelector("#likeButtonContainer"),
+      movie: {
+        id: movie.id,
+        title: movie.title,
+        overview: movie.overview,
+        backdrop_path: movie.backdrop_path,
+        vote_average: movie.vote_average,
+      },
+    });
   },
 };
 
