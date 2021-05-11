d77c22d3b5745a9029fd51c3ade67dc8661204f6
unknown <ijlnflhbrzdev@gmail.com>
2021-05-11 21:58:13 +0700

    Menambahkan Notifikasi pada window / laptop Sesuai dari data yg di terima oleh webpack setiap 10 detik sekali

--- src/scripts/views/templates/websocket-intiator.js
+++ src/scripts/views/templates/websocket-intiator.js
@@ -1,10 +1,20 @@
+import NotificationHelper from "../../utils/notification-helper";
+import CONFIG from "../../globals/config";
+
 const WebSocketInitiator = {
   init(url) {
     const webSocket = new WebSocket(url);
     webSocket.onmessage = this._onMesssageHandler;
   },
   _onMesssageHandler(message) {
-    console.log(message.data);
+    const movie = JSON.parse(message.data);
+    NotificationHelper.sendNotification({
+      title: `${movie.title} is on cinema!`,
+      options: {
+        body: movie.overview,
+        image: `${CONFIG.BASE_IMAGE_URL + movie.poster_path}`,
+      },
+    });
   },
 };
 export default WebSocketInitiator;
